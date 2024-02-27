ee526203590d7c92cf75e8856728172b16d9bcba
IngSis <IngSis@DESKTOP-70ESB6N>
2024-02-27 12:04:34 -0500

    Agregar archivos de proyecto.

---
+++ libCollections/pkgCollections/pkgLineal/pkgADT/clsADTVector.cs
@@ -0,0 +1,21 @@
+﻿using pkgServicies.pkgCollections.pkgLineal.pkgInterfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pkgServicies.pkgCollections.pkgLineal.pkgVector.pkgADT
+{
+    class clsADTVector<T>: clsADTLineal<T>, iADTVector<T> where T: IComparable<T>
+    {
+        protected int attCapacity;
+        protected T[] Items;
+
+        public int opGetCapacity()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
