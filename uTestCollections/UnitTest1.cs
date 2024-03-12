516132c51845d77c1a56e5332f1c31e83d1df09c
Sarah Quintero <sarahquintero11@outlook.es>
2024-03-12 11:53:50 -0500

    last savings

---
+++ uTestCollections/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace uTestCollections
+{
+    [TestClass]
+    public class uTestVectorStack
+    {
+        [TestMethod]
+        private clsVectorStack<int> testMyStack;
+
+        [TestMethod]
+        public void testDefaultConstructor()
+        {
+            #region Setup
+
+            #endregion
+            #region Test & Assert
+
+            #endregion
+        }
+        [TestMethod]
+        public void testValidCapacityConstructor()
+        {
+            #region Setup
+
+            #endregion
+            #region Test & Assert
+
+            #endregion
+        }
+        [TestMethod]
+        public void testNegativeCapacityConstructor()
+        {
+            #region Setup
+
+            #endregion
+            #region Test & Assert
+
+            #endregion
+        }
+        [TestMethod]
+        public void testOverFlowCapacityConstructor()
+        {
+            #region Setup
+
+            #endregion
+            #region Test & Assert
+
+            #endregion
+        }
+ 
+}
