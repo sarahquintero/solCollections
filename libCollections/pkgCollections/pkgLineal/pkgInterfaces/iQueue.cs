bb02183acfd5911e9431cf934cc86bc24bb96cfa
Sarah Quintero <sarahquintero11@outlook.es>
2024-03-12 11:53:50 -0500

    last savings

--- libCollections/pkgCollections/pkgLineal/pkgInterfaces/iQueue.cs
+++ libCollections/pkgCollections/pkgLineal/pkgInterfaces/iQueue.cs
@@ -8,8 +8,10 @@ namespace pkgServicies.pkgCollections.pkgLineal.pkgInterfaces
 {
     public interface iQueue<T> where T:IComparable<T>
     {
-        bool opPush(T prmItem);
+        #region CRUDs
+		bool opPush(T prmItem);
         bool opPop(ref T prmItem);
-        bool opPeek(ref T prmItem);
+        bool opPeek(ref T prmItem); 
+	#endregion
     }
 }
