336f704110b4c4dd36a99d1b0b599825d584f7dd
Sarah Quintero <sarahquintero11@outlook.es>
2024-03-12 11:53:50 -0500

    last savings

---
+++ uTestCollections/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("uTestCollections")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("uTestCollections")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0065a8f3-da92-454c-91b2-2a8007cdcd64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ uTestCollections/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace uTestCollections
+{
+    [TestClass]
+    public class uTestVectorStack
+    {
+        [TestMethod]
+        private clsVectorStack<int> testMyStack;
+        {
+        [TestMethod]
+        public 
+        }
+    }
+}
