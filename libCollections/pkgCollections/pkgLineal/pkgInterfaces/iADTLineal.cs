c60b0279a6ece96c3778d36c6ca730c481b17ac8
Sarah Quintero <sarahquintero11@outlook.es>
2024-03-12 11:53:50 -0500

    last savings

--- libCollections/pkgCollections/pkgLineal/pkgInterfaces/iADTLineal.cs
+++ libCollections/pkgCollections/pkgLineal/pkgInterfaces/iADTLineal.cs
@@ -8,12 +8,28 @@ namespace pkgServicies.pkgCollections.pkgLineal.pkgInterfaces
 {
     interface iADTLineal<T> where T : IComparable<T>
     {
-        bool opModify(int prmIdx, T prmItem);
-        bool opRetrieve(int prmIdx, ref T prmItem);
-        int opFind(T prmItem);
+        #region Getters
+		int opGetLength(T prmItem); 
+	#endregion
+        #region Query
+		int opFind(T prmItem);
         bool opExists(T prmItem);
-        int opGetLength(T prmItem);
-        T[] opToArray();
-        string opToString();
+        bool opItsEmpty();
+        bool opIsValid();
+        bool opItsOrderedAscending();
+        bool opItsOrderedDescending(); 
+	#endregion
+        #region Serialize
+		T[] opToArray();
+        string opToString(); 
+	#endregion
+        #region Deserialize
+		bool opToItems(T[] Array); 
+	#endregion
+        #region CRUDs
+		bool opModify(int prmIdx, T prmItem);
+        bool opRetrieve(int prmIdx, ref T prmItem); 
+	#endregion
+        
     }
 }
